29ac87f6060ab94da954123293afd7b9b6d94b4a
AlinaNerlich-dev <info@alina-nerlich.com>
2021-04-19 20:02:08 +0200

    colors and fontfamily

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,18 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     colors: {
-      
+      'dark-blue':'#262B46',
+      'white':'#FFFFFF',
+      'gin-light-blue':'#6FC3DF',
+      'vodka-purple':'#8D82C4',
+      'rum-red':'#EC8D81',
+      'scotch-yellow':'#E7B788',
+      'mocktail-violet':'#8EA9E8',
+      'random-green':'#87C5A4 '
+    },
+    fontFamily: {
+      serif: ['Playfair+Display'],
+      plain: ['Montserrat']
     },
     extend: {},
   },
@@ -12,3 +23,4 @@ module.exports = {
   },
   plugins: [],
 }
+ 
