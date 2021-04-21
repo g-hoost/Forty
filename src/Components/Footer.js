b9ea9e4a436baefe3c3efdd12f7ff0a60d529710
Georgia Hoost <gfroehling@icloud.com>
2021-04-21 20:06:40 +0200

    creating footer

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,5 +1,26 @@
-const Footer = () => {
-  return <h1>Footer</h1>;
+const Footer = () =>
+{
+  return
+  (
+    <footer>
+      <h3> FORTY</h3>
+      <p>MENU</p>
+      <h1>Cocktails & Getränke</h1>
+      <p>HERZLICH WILLKOMMEN IN DER WELT DER COCKTAILS UND GETRÄNKE!</p>
+      <br></br>
+      <input
+        type="search"
+        name="search"
+        value="type something"
+      // onChange={this.handleChange}
+      ></input>
+      <button>Search</button>
+      <div className="headerImgContainer">
+        <img src={vector} alt="three down arrows"></img>
+      </div>
+    </footer>
+  )
+
 };
 
 export default Footer;
