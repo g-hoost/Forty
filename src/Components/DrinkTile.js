f3862cd01203a9e9a48d0d04edc6c2bf8c10716a
AlinaNerlich-dev <info@alina-nerlich.com>
2021-04-23 20:27:00 +0200

    styling DrinkTile einigermaßen ;)

--- src/Components/DrinkTile.js
+++ src/Components/DrinkTile.js
@@ -4,15 +4,17 @@ import DrinkDetailPage from "./DrinkDetailPage";
 const DrinkTile = (props) =>
 {
   const currentURL = window.location.pathname;
-
+  
   return (
-    <Link to={`${currentURL}/${props.content.idDrink}`}>
-      <div className="flex direction">
-        <h2 className="transform -rotate-90  font-bold text-2xl w-full">{props.content.strDrink}</h2>
-        <img className="w-60" src={`${props.content.strDrinkThumb}`} alt="Cocktail Img" />
-      </div>
+    <Link className="md:flex md:flex-wrap md:inline-block" to={`${currentURL}/${props.content.idDrink}`}>
+        <div className="flex h-full items-center">
+          <h2 className="transform -rotate-90 font-bold text-2xl w-full">{props.content.strDrink}</h2>
+          <img className="w-60 p-6 object-contain" src={`${props.content.strDrinkThumb}`} alt="Cocktail Img" />
+        </div>
+
     </Link>
   );
 };
 
 export default DrinkTile;
+
