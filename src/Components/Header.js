5cda825a406dbe70f756434d19864731be93aa67
AlinaNerlich-dev <info@alina-nerlich.com>
2021-04-24 17:17:57 +0200

    own cocktail button

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -5,35 +5,17 @@ import {Link} from 'react-router-dom'
 
 const Header = () =>
 {
-  const openNav = () => {
-    document.getElementById("mySidenav").style.width = "250px";
-    document.getElementById("header").style.marginLeft = "250px";
-  }
-  
-  function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
-    document.getElementById("main").style.marginLeft = "0";
-  }
-
   return (
     <header className="bg-dark-blue p-3">
       <article className="text-sm flex justify-between">
         <Link to="/"><h3 className="font-secondary uppercase"> Forty</h3></Link>
-        <p onClick="openNav()" id="mySidenav" className="uppercase">Menu</p>
+        <p onClick=""id="mySidenav" className="uppercase">Menu</p>
       </article>
       <section className="mt-24 ml-12 text-left">
         <h1 className="font-secondary text-5xl">Cocktails & Getränke</h1>
         <p className="text-xs uppercase mt-2">Herzlich Willkommen in der Welt der Cocktails und Getränke!</p>
         <div className="text-dark-blue mt-6">
           <Searchbar />
-          {/* <input className="bg-white rounded-lg  text-center py-2"
-            type="text"
-            name="search"
-            value="type something"
-            size="15"
-          // onChange={this.handleChange}
-          ></input>
-          <button className="bg-light-blue rounded-lg px-2 py-2 ml-4">Search</button> */}
         </div>
       </section>
       <div className="headerImgContainer grid justify-center ">
