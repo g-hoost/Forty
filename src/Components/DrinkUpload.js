cef3cb2c80f52bed5467ff84a7436be9b5d8fe37
AlinaNerlich-dev <info@alina-nerlich.com>
2021-04-24 10:20:56 +0200

    inputfelder ohne value

--- src/Components/DrinkUpload.js
+++ src/Components/DrinkUpload.js
@@ -9,17 +9,17 @@ const DrinkUpload = () =>
       <section className="md:flex">
         <form className="text-dark-blue 
         lg:w-1/4">
-          <input className="upload-input " type="text" value="Name" placeholder="Name" />
-          <input className="upload-input" type="text" value="Kategorie" placeholder="Kategorie" />
-          <input className="upload-input" type="text" value="Bild URL" placeholder="Bild URL" />
-          <input className="upload-input  h-28" type="textarea" value="Anleitung" placeholder="Anleitung" />
-          <input className="upload-input" type="text" value="Zutat 1" placeholder="Zutat 1" />
-          <input className="upload-input" type="text" value="Menge Zutat 1" placeholder="Menge Zutat 1" />
-          <input className="upload-input" type="text" value="Zutat 2" placeholder="Zutat 2" />
-          <input className="upload-input" type="text" value="Menge Zutat 2" placeholder="Menge Zutat 2" />
-          <input className="upload-input" type="text" value="Zutat 3" placeholder="Zutat 3" />
-          <input className="upload-input" type="text" value="Menge Zutat 3" placeholder="Menge Zutat 3" />
-          <input className="bg-light-blue text-white font-bold mt-2 py-2 px-20 rounded-2xl w-full" type="submit" value="Submit" placeholder="Submit" />
+          <input className="upload-input " type="text" placeholder="Name" />
+          <input className="upload-input" type="text" placeholder="Kategorie" />
+          <input className="upload-input" type="text" placeholder="Bild URL" />
+          <input className="upload-input  h-28" type="textarea" placeholder="Anleitung" />
+          <input className="upload-input" type="text" placeholder="Zutat 1" />
+          <input className="upload-input" type="text" placeholder="Menge Zutat 1" />
+          <input className="upload-input" type="text" placeholder="Zutat 2" />
+          <input className="upload-input" type="text" placeholder="Menge Zutat 2" />
+          <input className="upload-input" type="text" placeholder="Zutat 3" />
+          <input className="upload-input" type="text" placeholder="Menge Zutat 3" />
+          <input className="bg-light-blue text-white font-bold mt-2 py-2 px-20 rounded-2xl w-full" type="submit" placeholder="Submit" />
         </form>
         <div className="md:w-3/4 md:relative">
           <img className="hidden md:block absolute top-0 w-72	" src="./../img/image9.png" alt="" />
