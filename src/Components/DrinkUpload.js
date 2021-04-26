522927c41f54e52bfeb8e6a407a9a26137e7c9a4
AlinaNerlich-dev <info@alina-nerlich.com>
2021-04-26 10:13:19 +0200

    CSS finetuning

--- src/Components/DrinkUpload.js
+++ src/Components/DrinkUpload.js
@@ -27,13 +27,14 @@ const DrinkUpload = () => {
   return (
 
     <article className="bg-dark-blue text-white p-12 text-center
-    md:text-left">
+                        md:text-left">
       <h1 className="font-secondary font-bold text-2xl mb-8
-      md:mb-14 md:text-3xl lg:text-left lg:mb-12
+                    md:mb-14 md:text-3xl 
+                    lg:text-left lg:mb-12
       ">Füge deine eigenen Getränke hinzu!</h1>
       <section className="md:flex">
-        <form className="text-dark-blue 
-        md:w-1/3">
+        <form className="text-dark-blue
+                        md:w-1/3">
           <input className="upload-input " type="text" placeholder="Name" />
           <select className="upload-input" name="Kategorie">
             <option value="Gin">Gin</option>
@@ -44,8 +45,8 @@ const DrinkUpload = () => {
           </select>
           <input className="upload-input" type="text" placeholder="Kategorie" />
           <input className="upload-input" type="text" placeholder="Bild URL" />
-          <input
-            className="upload-input  h-28"
+          <textarea
+            className="upload-input h-28"
             type="textarea"
             placeholder="Anleitung"
           />
@@ -77,10 +78,15 @@ const DrinkUpload = () => {
 
         <div className="md:w-2/3 md:relative md:w-full md:flex">
           <div className="md:w-1/2 md:relative">
-            <img className="hidden md:block absolute w-11/12 right-2 top-10 " src={image9} alt="" />
-            <img className="hidden md:block absolute w-9/12 right-2 bottom-20" src={image11} alt="" />
+            <img className="hidden absolute w-11/12 -right-2 top-10 
+                            md:block " src={image9} alt="" />
+            <img className="hidden absolute w-10/12 -right-2 bottom-32
+                            md:block " 
+                            src={image11} alt="" />
           </div>
-          <img className="hidden md:w-1/2 md:block absolute w-2/5 ml-4 -right-12 top-0 right-0" src={image10} alt="" />
+          <img className="hidden absolute w-2/5 ml-4 -right-12 top-0 right-0
+                          md:w-1/2 md:block " 
+                          src={image10} alt="" />
         </div>
       </section>
     </article>
